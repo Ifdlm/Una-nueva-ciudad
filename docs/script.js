db7c80b9cbc03231baa3cd504a1cd54ba9a8b963
Ifdlm <ignacio.flores@uc.cl>
2023-11-30 11:56:08 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,22 @@
-function redirectToYouTube() {
-    window.location.href = "https://www.youtube.com/watch?v=ecy1kkwR_kQ";
-}
-
-
 function changeSiteColor() {
     var body = document.body;
+    var header = document.querySelector('header');
 
-    // Obtener el color actual del fondo
-    var currentColor = window.getComputedStyle(body, null).getPropertyValue("background-color");
+    // Obtener el color actual del fondo del cuerpo y del encabezado
+    var bodyColor = window.getComputedStyle(body, null).getPropertyValue("background-color");
+    var headerColor = window.getComputedStyle(header, null).getPropertyValue("background-color");
 
-    // Cambiar el color del fondo
-    body.style.backgroundColor = (currentColor === "rgb(255, 255, 255)") ? "lightblue" : "white";
+    // Invertir colores del cuerpo y del encabezado
+    body.style.backgroundColor = (bodyColor === "rgb(255, 255, 255)") ? "darkgreen" : "white";
+    header.style.backgroundColor = (headerColor === "rgb(255, 255, 255)") ? "darkgreen" : "white";
+
+    // Invertir colores del texto en el encabezado
+    var title = document.querySelector('h1');
+    title.style.color = (headerColor === "rgb(255, 255, 255)") ? "white" : "green";
+}
+
+function redirectToYouTube() {
+    window.location.href = "https://www.youtube.com/watch?v=ecy1kkwR_kQ";
 }
 
 
