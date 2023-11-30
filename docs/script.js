945a4357fd39dd5aeb0519e054fd641418d299f7
Javmunoz <jmud@uc.cl>
2023-11-29 22:44:56 -0300

    Update script.js

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Obtén una referencia al botón por su ID
+const inscribeteBtn = document.getElementById('inscribeteBtn');
+
+// Agrega un evento de clic al botón
+inscribeteBtn.addEventListener('click', function() {
+    // Muestra un mensaje de alerta (puedes cambiar esto según tus necesidades)
+    alert('¡Gracias por tu interés en la campaña de reciclaje!');
+}); 
