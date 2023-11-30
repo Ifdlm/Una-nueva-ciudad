383496bb39d2f75718a9215b321068bfe109ef1a
Ifdlm <ignacio.flores@uc.cl>
2023-11-30 11:40:15 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,16 @@
 function redirectToYouTube() {
     window.location.href = "https://www.youtube.com/watch?v=ecy1kkwR_kQ";
 }
+
+
+function changeSiteColor() {
+    var body = document.body;
+
+    // Obtener el color actual del fondo
+    var currentColor = window.getComputedStyle(body, null).getPropertyValue("background-color");
+
+    // Cambiar el color del fondo
+    body.style.backgroundColor = (currentColor === "rgb(255, 255, 255)") ? "lightblue" : "white";
+}
+
+
