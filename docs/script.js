cf8ba89d07e8481ca49862885f8c3c2bdb15d7ee
Ifdlm <ignacio.flores@uc.cl>
2023-12-11 12:39:57 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,30 +1,15 @@
-function changeSiteColor() {
-    var body = document.body;
-    var header = document.querySelector('header');
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var imagenBanner = document.querySelector('.banner');
-    
-        imagenBanner.addEventListener('click', function() {
-            alert("Por un Chile más limpio nace como una iniciativa universitaria que busca a través de la información, generar conciencia sobre la problemática de los vertederos ilegales en nuestro país. Revisa nuestro sitio para conocer esta realidad.");
-        });
-    });
-    
-
-    // Obtener el color actual del fondo del cuerpo y del encabezado
-    var bodyColor = window.getComputedStyle(body, null).getPropertyValue("background-color");
-    var headerColor = window.getComputedStyle(header, null).getPropertyValue("background-color");
-
-    // Invertir colores del cuerpo y del encabezado
-    body.style.backgroundColor = (bodyColor === "rgb(255, 255, 255)") ? "darkgreen" : "white";
-    header.style.backgroundColor = (headerColor === "rgb(255, 255, 255)") ? "darkgreen" : "white";
-
-    // Invertir colores del texto en el encabezado
-    var title = document.querySelector('h1');
-    title.style.color = (headerColor === "rgb(255, 255, 255)") ? "white" : "green";
+// Función para mostrar mensaje al hacer clic en el banner
+function showWelcomeMessage() {
+    alert("Bienvenido a Por un Chile + Limpio, esta es una iniciativa universitaria que tiene como propósito concientizar respecto a una problemática poco abordada: los vertederos ilegales. Descubre nuestro sitio y mantente informado. Juntos, trabajamos por un Chile más limpio.");
 }
 
-function redirectToYouTube() {
-    window.location.href = "https://www.youtube.com/watch?v=ecy1kkwR_kQ";
+// Función para visitar el canal de YouTube
+function visitYouTube() {
+    window.location.href = "https://youtu.be/ecy1kkwR_kQ?si=skHEe3QWEsy2f_J_";
 }
 
+// Función para alternar la visibilidad del menú en dispositivos móviles
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'block' : 'none';
+}
